1e112f57b1f6ae90beac79a92dcd0f6532113584
Haleema-sadia-00 <i.m.haleema00@gmail.com>
2025-12-05 18:52:48 +0500

    Added PlatformContentScreen and StorageSelectionDialog with complete download flow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -14,7 +14,7 @@ import '../lib/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const TapMateApp());
+    await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
